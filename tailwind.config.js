8a02932c77bc64a32b8192d307c4abcf51052f35
Luis Gabriel <janco7249@gmail.com>
2022-04-20 00:19:02 -0400

    feat(Root): Added defaults colors in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        discord_blue: '#404eed',
+        discord_blue_btn: '#5865f2',
+        discord_blurple: '#7289da',
+        discord_purple: '#8565f2',
+        discord_green: '#3ba55c'
+      }
+    },
   },
   plugins: [],
 }
