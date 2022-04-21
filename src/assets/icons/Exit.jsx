839217862b91c535cabc8a6c9734ad7fc3197fab
Luis Gabriel <janco7249@gmail.com>
2022-04-20 22:00:01 -0400

    feat(Root): Add icon Exit

--- src/assets/icons/Exit.jsx
+++ src/assets/icons/Exit.jsx
@@ -1,7 +1,7 @@
 export const Exit = (props) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path d="M6 18L18 6M6 6l12 12" />
     </svg>
   )
 }
