53d716e9955a7c86607bb477c0427ff5022ab1bf
Luis Gabriel <janco7249@gmail.com>
2022-04-20 22:04:35 -0400

    test(Root): Update test when clicking button

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, prettyDOM, render, screen } from '@testing-library/react'
+import { hrefs } from '../../assets/data/hrefs'
+import Togglable from '../Togglable/Togglable'
 import Header from './Header'
 
 describe('Header test', () => {
-  test('renders', () => {
+  test('renders content', () => {
     render(<Header />)
+    hrefs.map((a) => {
+      const links = screen.getAllByText(a)
+      return expect(links).toHaveLength(2)
+    })
+    const button = screen.getByText(/Login/i)
+    expect(button).toBeInTheDocument()
+  })
+
+  test('clicking the button calls event handler once', () => {
+    const mockHandler = jest.fn()
+
+    render(<Header signIn={mockHandler} />)
+
+    const button = screen.getByText(/Login/i)
+    expect(button).toBeInTheDocument()
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(1)
+    // expect(mockHandler).toHaveBeenCalledTimes(1)
+  })
+
+  test('after clicking its must be shown a to menu modal', () => {
+    render(
+      <Togglable stringStyle='text-white' >
+        <span className='text-base'>testSpanContent</span>
+      </Togglable >
+    )
+    const { parentNode } = screen.getByText(/testSpanContent/i)
+    console.log(prettyDOM(parentNode))
+    fireEvent.click(parentNode)
   })
 })
