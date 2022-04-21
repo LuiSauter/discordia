c8e21dc41ab25d622e20205742c4db9dc6f53393
Luis Gabriel <janco7249@gmail.com>
2022-04-20 21:58:21 -0400

    test(Root): Clean test to expect in test of App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders react clone discord', () => {
+test('renders react app', () => {
   render(<App />);
-  const linkElement = screen.getByTitle(/Discordia/i);
-  expect(linkElement).toBeInTheDocument();
 });
