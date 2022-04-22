3ab42ddf524c5a0d39b3df8cb37c58d7cf2106ff
Luis Gabriel <janco7249@gmail.com>
2022-04-21 21:24:27 -0400

    feat(Root): Add more icons

---
+++ src/assets/icons/Download.jsx
@@ -0,0 +1,7 @@
+export const Download = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+    </svg>
+  )
+}
