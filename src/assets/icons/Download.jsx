d5afb94998f49e065f76ab8e8e5befd5e8ddd4ce
Luis Gabriel <janco7249@gmail.com>
2022-04-22 22:56:17 -0400

    feat(Root): Change style of Exit icon

--- src/assets/icons/Download.jsx
+++ src/assets/icons/Download.jsx
@@ -1,7 +1,10 @@
-export const Download = () => {
+export const Download = ({ title = 'Download', titleId = '' }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+    <svg className='h-6 w-6 mr-1' aria-labelledby={titleId}>
+      {title ? <title id={titleId}>{title}</title> : null}
+      <g fill="currentColor">
+        <path d="m17.707 10.708-1.414-1.414L13 12.587V2.001h-2v10.586L7.707 9.294l-1.414 1.414L12 16.415l5.707-5.707ZM18 18.001v2H6v-2H4v2c0 1.102.897 2 2 2h12c1.104 0 2-.898 2-2v-2h-2Z" />
+      </g>
     </svg>
   )
 }
