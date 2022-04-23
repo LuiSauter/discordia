14a0e0e192022a6dff649c61ec194b7c834bd0c8
Luis Gabriel <janco7249@gmail.com>
2022-04-22 22:57:55 -0400

    feat(Root): Change style of Exit icon

--- src/assets/icons/Exit.jsx
+++ src/assets/icons/Exit.jsx
@@ -1,7 +1,18 @@
-export const Exit = (props) => {
+export const Exit = ({ title = 'Exit', titleId = '' }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path d="M6 18L18 6M6 6l12 12" />
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      viewBox="0 0 512 512"
+      className="h-6 w-6"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-labelledby={titleId}
+    >
+      {title ? <title id={titleId}>{title}</title> : null}
+      <path
+        d="M405 136.798 375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"
+        stroke="none"
+      />
     </svg>
   )
 }
