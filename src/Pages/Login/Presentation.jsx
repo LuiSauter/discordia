af6cbbbaadd810325b292876928e2ffd39004e79
Luis Gabriel <janco7249@gmail.com>
2022-04-22 22:55:41 -0400

    feat(Root): Add data for show presentation of app and styles

--- src/Pages/Login/Presentation.jsx
+++ src/Pages/Login/Presentation.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { presentation } from '../../assets/data/presentation'
 
 const Presentation = () => {
   return (
-    <div className='h-screen text-3xl text-center'>
-      <p>Presentation</p>
-      <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-    </div>
+    <section className='w-full'>
+      {presentation.map((card, index) => (
+        <article
+          key={index}
+          className={`${index % 2 !== 0 && 'bg-[#f6f6f6]'} px-6 py-14 md:px-10 md:py-20 h-full lg:min-h-screen 2xl:min-h-full w-full flex flex-col items-center justify-center`}
+        >
+          <div className={`${index % 2 !== 0 ? 'lg:grid-cols-[1fr_1.5fr]' : 'lg:grid-cols-[1.5fr_1fr]'} max-w-7xl h-full grid grid-cols-1 md:grid-cols-2 items-center md:gap-4 w-full`}>
+            <div className={`${index % 2 !== 0 ? 'md:order-1' : ''} w-full relative mt-6`}>
+              {card.svg}
+            </div>
+            <section className='pt-5 w-full lg:max-w-[380px] mx-auto'>
+              <h2 className='text-2xl font-bold leading-7 md:text-5xl w-full'>{card.title}</h2>
+              <footer className='pt-6 md:text-base lg:text-xl w-full'>{card.description}</footer>
+            </section>
+          </div>
+        </article>
+      ))}
+    </section>
   )
 }
 
---
+++ src/assets/data/presentation.js
@@ -0,0 +1,20 @@
+import { First } from "../icons/presentation/First";
+// import { Fourth } from "../icons/presentation/Fourth";
+import { Second } from "../icons/presentation/Second";
+import { Third } from "../icons/presentation/Third";
+
+export const presentation = [
+  {
+    title: 'Create an invite-only place where you belong',
+    description: 'Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.',
+    svg: <First />
+  }, {
+    title: 'Where hanging out is easy',
+    description: 'Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.',
+    svg: <Second />
+  }, {
+    title: 'From few to a fandom',
+    description: 'Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more.',
+    svg: <Third />
+  },
+]
