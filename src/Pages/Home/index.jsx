ae0d03b1eee0090444ae51424d8bd54336a8270c
Luis Gabriel <sauter@sauter-desktop.localdomain>
2022-04-25 23:14:39 -0400

    feat(Root): Add redirect root / in Home if unauthenticated

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useLocation } from 'wouter'
+import ServerIcon from '../../components/ServerIcon'
+import { auth } from '../../lib/firebase'
+
+const Home = () => {
+  const [user] = useAuthState(auth)
+  const [, setLocation] = useLocation()
+  return (
+    <>
+      {!user && setLocation('/', { replace: true })}
+      <div className='flex h-screen bg-[#222]'>
+        <nav>
+          <div>
+            <img src="https://rb.gy/kuaslg" alt="discordia" className='h-5' />
+          </div>
+          <hr className='border-gray-700 border w-8 mx-auto bg-gray-700' />
+          <ServerIcon image='https://rb.gy/qidcpp' />
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default Home
