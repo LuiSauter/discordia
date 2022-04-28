9bc386c6b5aba9d65556f51f1cc3de960e54b195
Luis Gabriel <sauter@sauter-desktop.localdomain>
2022-04-27 21:04:01 -0400

    feat(Root): Adding more icons component

---
+++ src/assets/icons/ArrowDown.jsx
@@ -0,0 +1,14 @@
+export const ArrowDown = ({ styleString }) => (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    className={styleString}
+    strokeWidth={2}
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="m6 9 6 6 6-6" />
+  </svg>
+)
