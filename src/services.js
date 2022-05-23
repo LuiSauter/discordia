53db1f91378eeebd3d3cd0a9c698dc3facf31f95
Luis Gabriel <janco7249@gmail.com>
2022-05-23 19:54:58 -0400

    feat(Root): Create services to connect database res-api

---
+++ src/services.js
@@ -0,0 +1,24 @@
+const { REACT_APP_URL_DATABASE, REACT_APP_END_POINT_USER } = process.env
+
+export const loginWithGoogle = async ({ username = '', email = '', photoUrl = '' }) => {
+  try {
+    const login = await fetch(`${REACT_APP_URL_DATABASE + REACT_APP_END_POINT_USER}/auth`, {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, email, photoUrl })
+    })
+    const data = await login.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.error(error.message)
+    return { status: 'hey sauter, there is an error!.' }
+  }
+}
+
+export const getUser = async ({ username = 'sauter' }) => {
+  const user = await fetch(`${REACT_APP_URL_DATABASE + REACT_APP_END_POINT_USER}/${username}`)
+  const data = await user.json()
+  return data
+}
