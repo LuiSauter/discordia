fadd2a3a09b876e48133917169700ecbe76c0570
Luis Gabriel <janco7249@gmail.com>
2022-05-27 15:33:10 -0400

    feat(Root): Change style in channel and loadingHome

--- src/components/LoadingHome.jsx
+++ src/components/LoadingHome.jsx
@@ -3,7 +3,7 @@ import { LoadingIcon } from '../assets/icons/Loading'
 
 const LoadingHome = () => {
   return (
-    <section className='h-screen w-screen grid place-content-center place-items-center bg-discord_channels_bg overflow-hidden select-none'>
+    <section className='fixed inset-0 z-30 h-screen w-screen grid place-content-center place-items-center bg-discord_channels_bg overflow-hidden select-none'>
       <img className='w-20 h-14 relative' src="https://i1.wp.com/pngkey.com/png/full/20-200938_discord-png.png" alt="" />
       <p className='text-white w-72 text-center pt-4'>Welcome to Discord the clone of Discord, <span className='font-semibold'>developed by Sauterdev</span></p>
       <div className='w-full grid place-content-center place-items-center text-discord_blurple pt-4'>
