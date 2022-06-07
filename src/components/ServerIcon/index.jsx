002ddd8bcc2bdb1159a18cf42fa3f9aed8af3f02
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-06 22:51:46 -0400

    fix(Root): Add error handler on image of servers

--- src/components/ServerIcon/index.jsx
+++ src/components/ServerIcon/index.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from 'react'
-import { useLocation } from 'wouter'
-import { useRoute } from 'wouter'
+import { useState } from 'react'
+import { useLocation, useRoute } from 'wouter'
+import avatar from '../../assets/images/avatar-default.svg'
 
 const ServerIcon = ({ image = '', serverId = '' }) => {
-  const [match, params] = useRoute('/channels/:serverId/:id')
-  const [location, setLocation] = useLocation()
+  const [, params] = useRoute('/channels/:serverId/:id') // [match, params]
+  const [, setLocation] = useLocation()
   const [mouseHover, setMouseHover] = useState(false)
-
 
   const { serverId: serverIdParams } = params || { serverId: '' }
 
-  return (
-    <div className='relative flex flex-row w-full'>
-      <span className={`absolute left-0 h-5 ${serverIdParams !== serverId && mouseHover ? 'scale-y-100 top-4' : serverIdParams === serverId ? 'scale-y-100 h-10 top-1' : 'scale-y-0 top-4'} transition-transform duration-300 ease-out w-[5px] bg-white rounded-r-2xl`} />
+  const handleImgError = ({ currentTarget }) => {
+    currentTarget.onerror = null
+    currentTarget.src = avatar
+  }
 
+  return (
+    <>
+      <span className={`absolute left-0 h-5 ${serverIdParams !== serverId && mouseHover ? 'scale-y-100 top-4' : serverIdParams === serverId ? 'scale-y-100 h-10 top-1' : 'scale-y-0 top-4'} transition-transform duration-300 ease-out w-[5px] bg-white rounded-r-2xl`} />
       <button onMouseEnter={() => setMouseHover(true)} onMouseLeave={() => setMouseHover(false)} onClick={() => setLocation(`/channels/${serverId}/asss123`)} className={`server-default min-h-[48px] w-[48px] mx-auto ${serverIdParams === serverId ? 'rounded-2xl' : 'server-icon'} overflow-hidden`}>
         <img
           src={image}
+          onError={handleImgError}
           alt="server discordia"
           className='server-default w-12 object-cover'
         />
       </button>
-    </div>
+    </>
   )
 }
 
