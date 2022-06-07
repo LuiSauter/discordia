b12773f0290719e5322e706c2380202c12124a74
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-06 22:36:26 -0400

    chore(Root): Add custom eslit standard

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    indent: ['warn', 2],
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-indent': [2, 2, { indentLogicalExpressions: true }],
+    "react/jsx-indent-props": [2, 2],
+    'no-unused-vars': 'warn',
+    'react/prop-types': 0 // disable prop-types
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
