1038e60bba06621c3246b7eebe025d6c1b62afd8
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-06 22:33:08 -0400

    feat(Root): Add service to fetch post `createServer`

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,5 @@ export const dbConstants = {
   dbApiUri: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_URL_DATABASE : 'http://localhost:4000',
   dbEndPointUser: process.env.REACT_APP_END_POINT_USER,
   dbEndPointChannel: process.env.REACT_APP_END_POINT_CHANNEL,
+  dbEndPointServer: process.env.REACT_APP_END_POINT_SERVER
 }
