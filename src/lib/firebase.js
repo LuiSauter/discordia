ed067755932a632fe40505308ba642257290b570
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-16 23:41:30 -0400

    refactor(Root): Replace environment variables for constants for secrets .env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { dbConstants } from '../utils/constants';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: dbConstants.dbAuthApiKey,
+  authDomain: dbConstants.dbAuthAuthDomain,
+  projectId: dbConstants.dbAuthProjectId,
+  storageBucket: dbConstants.dbAuthStorageBucket,
+  messagingSenderId: dbConstants.dbAuthMessagingSenderId,
+  appId: dbConstants.dbAuthAppId
 };
 
 initializeApp(firebaseConfig)
