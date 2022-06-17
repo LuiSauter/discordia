3e047fb6db985f2da601683356c3752ac624e044
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-16 23:43:50 -0400

    refactor(Root): Remove unusable variables

--- src/components/Header/ButtonLogin/index.jsx
+++ src/components/Header/ButtonLogin/index.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useLocation } from 'wouter'
 import { auth, signInWithPopup, provider } from '../../../lib/firebase'
