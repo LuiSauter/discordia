fe261f5e9b194c577905759a3e23b307084105b5
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-18 23:58:06 -0400

    feat(Root): Create reusable Component for chat section

---
+++ src/components/chat/Container.jsx
@@ -0,0 +1,25 @@
+import { Menu } from '../../assets/icons/action'
+import { useToggle } from '../../hooks/useToggle'
+
+const Container = ({ children, header = 'Discordia', aside = false }) => {
+  const { toggleMenu, activeMenu } = useToggle()
+  return (
+    <section className={`bg-discord_hover h-full w-full overflow-y-hidden relative flex flex-col md:pl-60 ${activeMenu && 'ml-[230px] sm:ml-[250px] min-w-[400px] scale-95 rounded-xl md:ml-0 md:scale-100 md:min-w-min md:rounded-none transition-all duration-100'} transform transition-transform duration-100`}>
+      {activeMenu && <div className='absolute top-0 left-0 w-full h-full bg-discord_nav_server opacity-80 z-10 cursor-pointer md:hidden' onClick={toggleMenu} />}
+      <header className='px-4 w-full border-b-2 border-discord_nav_server/50 hover:bg-discord_hover flex flex-row items-center justify-start gap-4 text-white transition-all'>
+        <button onClick={toggleMenu} className='md:hidden hover:bg-discord_inputChat transition-colors duration-75 ease-out rounded-md px-2 py-[6px]'>
+          <Menu />
+        </button>
+        <h3 className='text-white text-base font-bold my-3'>{header}</h3>
+      </header>
+      <main className='h-full w-full overflow-hidden flex flex-row relative text-white'>
+        {children}
+        {aside && <aside className='hidden w-56 lg:flex flex-shrink-0 h-full bg-discord_channels_bg transition-all duration-150'>
+          {aside}
+        </aside>}
+      </main>
+    </section>
+  )
+}
+
+export default Container
