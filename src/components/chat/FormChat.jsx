fd8358f92cb1d4481faa233119c61a4ed8dd12d4
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-19 00:00:37 -0400

    feat(Root): Add method to create messages of channel

--- src/components/chat/FormChat.jsx
+++ src/components/chat/FormChat.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import TextareaAutosize from 'react-textarea-autosize'
 import { Emoji, Gif, Gift, PlusAction } from '../../assets/icons/action'
+import { sendMessage } from '../../store/slices/channel'
 
-const FormChat = ({ username = '@username' }) => {
+const FormChat = ({ channelId = '', username = '@username' }) => {
   const [message, setMessage] = useState('')
+  const dispatch = useDispatch()
+  const { data } = useSelector(state => state.user)
+  const { data: dataChannel } = useSelector(state => state.channel)
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
-      console.log('Handler submit')
+      e.preventDefault()
+      const yourId = dataChannel.owner.find(user => user._id !== data?._id)?._id
+      dispatch(sendMessage({ message, channelId, myId: data?._id, yourId }))
+      setMessage('')
     }
   }
 
@@ -17,18 +25,29 @@ const FormChat = ({ username = '@username' }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(e.target, message)
   }
+
   return (
-    <div className='h-max w-full bg-discord_server absolute bottom-0 left-0'>
+    <div className='h-max w-full bg-discord_server sticky bottom-0 left-0'>
       <form onSubmit={handleSubmit} className='w-full flex justify-center px-4 pb-4 items-center h-full'>
         <div className='flex flex-row items-start w-full rounded-xl bg-discord_inputChat overflow-hidden'>
           <button className='w-14 flex text-discord_inputChat items-center justify-center px-4 py-3'><PlusAction /></button>
-          <TextareaAutosize onKeyDown={handleKeyDown} onChange={handleChange} maxRows={10} className='bg-discord_inputChat w-full resize-none outline-none flex flex-row py-[9px] text-base text-white' placeholder={`Enviar un mesage a @${username}`} />
+          <TextareaAutosize
+            onKeyDown={handleKeyDown}
+            value={message}
+            onChange={handleChange}
+            cols={1}
+            maxRows={10}
+            className='bg-discord_inputChat w-full resize-none outline-none flex flex-row py-[9px] text-base text-white'
+            placeholder={`Enviar un mesage a @${username}`}
+          />
           <div className='flex flex-row pr-2'>
-            <button onClick={(e) => e.preventDefault()} className='w-min flex items-center justify-center px-2 py-3'><Gift /></button>
-            <button onClick={(e) => e.preventDefault()} className='hidden w-min sm:flex items-center justify-center px-2 py-2'><Gif /></button>
-            <button onClick={(e) => e.preventDefault()} className='w-min flex items-center justify-center px-2 py-3'><Emoji /></button>
+            <button onClick={(e) => e.preventDefault()} className='hidden w-min sm:flex items-center justify-center px-2 py-3'>
+              <Gift />
+            </button>
+            <button type='button' onClick={(e) => e.preventDefault()} className='w-min flex items-center justify-center px-2 py-3'>
+              <Emoji />
+            </button>
           </div>
         </div>
       </form>
--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -1,18 +1,24 @@
 import useTimeAgo from "../../hooks/useTimeAgo"
+import Image from "../Image"
 
 const Message = ({ id = '', author = '', message = '', createdAt = Date.now() }) => {
   const { timeago, hourAndMinute } = useTimeAgo(new Date(createdAt))
+
   return (
-    <li id={id} className='flex flex-row items-start gap-4 hover:bg-discord_channels_bg px-4 transition-colors duration-100 ease-out'>
+    <li id={id} className='relative flex flex-row items-start gap-4 hover:bg-discord_channels_bg px-4 transition-colors duration-100 ease-out'>
       <figure className='relative rounded-full w-10 h-10 flex-shrink-0 my-1 select-none'>
-        <img src={author.photoUrl} alt={author.username} className='rounded-full w-full h-full' />
+        <Image img={author.photoUrl} textAlt={author.username} classStyle='rounded-full w-full h-full' />
       </figure>
       <div className='flex flex-col'>
         <h2 className='flex flex-row items-center gap-2'>
           <span className='cursor-pointer text-[14px] font-medium'>{author.username}</span>
           <time dateTime={hourAndMinute} title={hourAndMinute} className='select-none text-[13px] font-extralight'>{timeago}</time>
         </h2>
-        <p className='text-slate-200 font-light'>{message}</p>
+        <p className='flex flex-col'>
+          {message.split('\n').map((msg, index) => (
+            msg === '' ? <br /> : <span key={index} className='text-slate-200 font-light'>{msg}</span>
+          ))}
+        </p>
       </div>
     </li>
   )
