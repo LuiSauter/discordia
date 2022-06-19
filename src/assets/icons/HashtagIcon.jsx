57f7653634c6c2e6a832247f18c1597cde48ac04
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-18 23:54:41 -0400

    feat(Root): Add more icons and images

--- src/assets/icons/HashtagIcon.jsx
+++ src/assets/icons/HashtagIcon.jsx
@@ -1,6 +1,6 @@
-export const HashtagIcon = () => {
+export const HashtagIcon = ({ classStyle = 'h-6 w-6' }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg xmlns="http://www.w3.org/2000/svg" className={classStyle} fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" />
     </svg>
   )
