2e1000c7b92882cc37def1f07cfce840815fc8d4
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-20 22:16:48 -0400

    feat(Root): Add more icons for menu actions

--- src/assets/icons/action/index.jsx
+++ src/assets/icons/action/index.jsx
@@ -102,3 +102,33 @@ export const Menu = () => (<svg
 >
   <path d="M3 12h18M3 6h18M3 18h18" />
 </svg>)
+
+export const DotsHorizontal = () => (<svg
+  stroke="currentColor"
+  fill="none"
+  strokeWidth={1}
+  viewBox="0 0 24 24"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+  className="h-4 w-4 scale-125"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path d="M0 0h24v24H0z" stroke="none" />
+  <circle cx={5} cy={12} r={1} />
+  <circle cx={12} cy={12} r={1} />
+  <circle cx={19} cy={12} r={1} />
+</svg>)
+
+export const PencilEdit = () => (<svg
+  stroke="currentColor"
+  fill="currentColor"
+  strokeWidth={0}
+  viewBox="0 0 16 16"
+  className="h-4 w-3 scale-110"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
+    stroke="none"
+  />
+</svg>)
